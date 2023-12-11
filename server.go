9d649e1d73c9d367364ba588168a910d8f37eced
gulugulu1103 <gulugulu1103@qq.com>
2023-12-12 00:28:43 +0800

    Add ListenMessage and BroadCast functions to Server

--- server.go
+++ server.go
@@ -27,17 +27,40 @@ func NewServer(ip string, port int) *Server {
 	}
 }
 
-func (this *)
+// ListenMessage 监听Message广播消息Channel的goroutine，一旦有消息就发送给全部的在线User
+func (this *Server) ListenMessage() {
+	for {
+		msg := <-this.Message
+
+		// 将msg发送给全部的在线User
+		this.mapLock.Lock()
+		for _, client := range this.OnlineMap {
+			client.C <- msg
+		}
+		this.mapLock.Unlock()
+	}
+}
+
+// BroadCast ListenMessage 监听Message广播消息Channel的goroutine，一旦有消息就发送给全部的在线User
+func (this *Server) BroadCast(user *User, msg string) {
+	sendMsg := "[" + user.Addr + "]" + user.Name + ":" + msg
+
+	this.Message <- sendMsg
+
+}
 
 func (this *Server) Handler(conn net.Conn) {
 	// 当前链接的业务
-	fmt.Println("链接建立成功")
+	//fmt.Println("链接建立成功")
 
 	// 用户上线，将用户加入到OnlineMap中
 	user := NewUser(conn)
 	this.mapLock.Lock()
 	this.OnlineMap[user.Name] = user
 	this.mapLock.Unlock()
+
+	this.BroadCast(user, "已上线")
+	select {}
 
 }
 
@@ -57,6 +80,9 @@ func (this *Server) Start() {
 			return
 		}
 	}(listener)
+
+	// 启动监听Message的goroutine
+	go this.ListenMessage()
 
 	for {
 		conn, err := listener.Accept()
